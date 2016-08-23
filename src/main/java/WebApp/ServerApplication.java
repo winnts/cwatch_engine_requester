9b0f7f4f66a900b6212a9529eb3de6c46b173249
Andrey Dyachenko <winnts@gmail.com>
2016-08-23 09:34:23 +0300

    web app sceleton added

---
+++ src/main/java/WebApp/ServerApplication.java
@@ -0,0 +1,14 @@
+package WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Andrey.Dyachenko on 20.08.16.
+ */
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
